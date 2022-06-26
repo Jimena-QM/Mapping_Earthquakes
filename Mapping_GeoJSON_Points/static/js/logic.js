0624b4c97176efab130a00ff21abdd5163f30f2d
Jimena-QM <jimenaqm@gmail.com>
2022-06-26 12:40:28 -0500

    GeoJSON

---
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -0,0 +1,67 @@
+// Add console.log to check if the code is working
+console.log("working");
+
+// Create map object with a center and zoom level using setView
+// setView([latitud, longiute], zoom level) zoom level on a scale of 0 to 18
+    // let map = L.map('mapid').setView([40.7, -94.5],4);
+    //let map = L.map('mapid', {center: [37.5, -122.5],zoom: 10});
+// Create map object with a center and zoom level without setView
+// This method is useful when we need to add multiple tile layers, or a background image of our map
+
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
+
+// Add GeoJSON data
+let sanFranAirport=
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]  
+            //GeoJSON data coordinates are set with the first parameter as X (longitude)
+            //and the second parameter as Y (latitude)
+        }
+}]};
+
+// Grabbing our GeoJSON data
+    // Using pointToLayer and .binPopup
+// L.geoJSON(sanFranAirport, {
+//     // Turn each feature into a marker on the map
+//     pointToLayer: function(feature, latlng){
+//         console.log(feature);
+//         return L.marker(latlng)
+//         .bindPopup("<h2>" + feature.properties.name + "</h2> <hr> <h3> " + feature.properties.city +
+//         ", " + feature.properties.country + "</h3>");
+//     }
+
+// }).addTo(map);
+
+L.geoJSON(sanFranAirport, {
+    // Turn each feature into a marker on the map
+    onEachFeature: function(feature, layer) {
+        console.log(layer);
+        layer.bindPopup("<h2>Airport code: " + feature.properties.faa + "</h2> <hr> <h3>Airport name: "
+        + feature.properties.name + "</h3>");    
+    }
+
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map. navigation-preview-night-v4/ light-v10 / dark-v10 / satellite-streets-v11 / streets-v11 / satellite-v9
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 14,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -27,21 +27,21 @@ L.polyline(line, {
 }).addTo(map);
 
 // Get data from cities.js
-// let cityData = cities;
+let cityData = cities;
 
 // Loop through the cities array and create one marker for each city
 
-// cityData.forEach(function(city) {
-//     console.log(city)
-//     L.circleMarker(city.location, {
-//         radius: city.population/200000,
-//         color: "orange",
-//         fillColor: "#ffffa1"
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        fillColor: "#ffffa1"
 
-//     })
-//     .bindPopup("<h2>" + city.city +", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-//     .addTo(map);
-// });
+    })
+    .bindPopup("<h2>" + city.city +", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
 
 // We create the tile layer that will be the background of our map. streets-v11 / dark-v10 / satellite-streets-v11
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
