dbe639c0a291c24a54509e88b86b87369a0aa954
Jimena-QM <jimenaqm@gmail.com>
2022-06-26 10:59:30 -0500

    Added mappings

---
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -0,0 +1,42 @@
+// Add console.log to check if the code is working
+console.log("working");
+
+// Create map object with a center and zoom level using setView
+// setView([latitud, longiute], zoom level) zoom level on a scale of 0 to 18
+    // let map = L.map('mapid').setView([40.7, -94.5],4);
+
+// Create map object with a center and zoom level without setView
+// This method is useful when we need to add multiple tile layers, or a background image of our map
+
+let map = L.map('mapid', {
+    center: [
+        34.0522, -118.2437
+    ],
+    zoom: 14
+});
+
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city
+
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {
+        radius: city.population/200000,
+        color: "orange",
+        fillColor: "#ffffa1"
+
+    })
+    .bindPopup("<h2>" + city.city +", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
+
+// We create the tile layer that will be the background of our map. streets-v11
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 4,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
---
+++ Mapping_Single_Points/static/js/logic 1.js
@@ -0,0 +1,33 @@
+// Add console.log to check if the code is working
+console.log("working");
+
+// Create map object with a center and zoom level using setView
+// setView([latitud, longiute], zoom level) zoom level on a scale of 0 to 18
+    // let map = L.map('mapid').setView([40.7, -94.5],4);
+
+// Create map object with a center and zoom level without setView
+// This method is useful when we need to add multiple tile layers, or a background image of our map
+
+let map = L.map('mapid', {
+    center: [
+        34.0522, -118.2437
+    ],
+    zoom: 14
+});
+
+// Add a marker to the map for LA, CA
+L.circleMarker([34.0522, -118.2437],{
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffffa1'
+}).addTo(map);
+
+// We create the tile layer that will be the background of our map. streets-v11
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.dark',
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
